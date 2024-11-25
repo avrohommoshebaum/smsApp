1250ffde9a536eea7ce4d42e554c1ee5706d1209
avrohommoshebaum <163787327+avrohommoshebaum@users.noreply.github.com>
2024-11-25 02:56:03 -0500

    Update messages.creat recievetextMessage.js

--- server/utils/smsFunctions/recievetextMessage.js
+++ server/utils/smsFunctions/recievetextMessage.js
@@ -28,7 +28,7 @@ const recieveTextMessage = async  (req, res) => {
                     const accountSid = process.env.TWILIO_ACCOUNT_SID;
                     const authToken = process.env.TWILIO_AUTH_TOKEN;
                     const client = twilio(accountSid, authToken);
-                await client.message.create({
+                await client.messages.create({
                     body: messageToSend, 
                     from: process.env.TWILIO_PHONE_NUMBER, 
                     to: number
